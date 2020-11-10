bd73ecc81b607216918c5fcec92bdfc0a2f3593c
letitride <ichikawa.fumiya@gmail.com>
2020-11-11 08:49:55 +0900

    libクレートの作成

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::io::{BufRead};
+
+
+pub fn count(input: impl BufRead) -> HashMap<String, usize> {
+  //w:単語構成文字とマッチする
+  let re = Regex::new(r"\w+").unwrap();
+  let mut freqs = HashMap::new();
+
+  for line in input.lines() {
+      let line = line.unwrap();
+      for m in re.find_iter(&line) {
+          let word = m.as_str().to_string();
+          *freqs.entry(word).or_insert(0) += 1;
+      }
+  }
+  freqs
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
 use std::env;
 use std::fs::File;
-use std::io::{BufReader, BufRead};
-use regex::Regex;
-use std::collections::HashMap;
+use std::io::{BufReader};
 
-pub fn count(input: impl BufRead) -> HashMap<String, usize> {
-    //w:単語構成文字とマッチする
-    let re = Regex::new(r"\w+").unwrap();
-    let mut freqs = HashMap::new();
-
-    for line in input.lines() {
-        let line = line.unwrap();
-        for m in re.find_iter(&line) {
-            let word = m.as_str().to_string();
-            *freqs.entry(word).or_insert(0) += 1;
-        }
-    }
-    freqs
-}
+use wordcount::count;
 
 fn main() {
     let filename = env::args().nth(1).expect("1 argument FILENAME  require");
