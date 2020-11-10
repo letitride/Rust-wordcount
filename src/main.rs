e0d19acf34bc07c38822acbff270daa3f2ac812f
letitride <ichikawa.fumiya@gmail.com>
2020-11-10 10:47:46 +0900

    BufReaderを使用したファイルの読み込み

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::env;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
 
 fn main() {
     let filename = env::args().nth(1).expect("1 argument FILENAME  require");
     println!("{}", filename);
+
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(&file);
+    for line in reader.lines() {
+        println!("{}", line.unwrap());
+    }
 }
