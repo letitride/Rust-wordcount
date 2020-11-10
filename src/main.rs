d065c98ba82827aec6c4cbd4a94f5d985ec3c412
letitride <ichikawa.fumiya@gmail.com>
2020-11-10 10:51:15 +0900

    単語数解析関数: countの雛形を作成

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ use std::env;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 
+fn count(input: impl BufRead) {
+    for line in input.lines() {
+        println!("{}", line.unwrap());
+    }
+}
+
 fn main() {
     let filename = env::args().nth(1).expect("1 argument FILENAME  require");
     println!("{}", filename);
 
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(&file);
-    for line in reader.lines() {
-        println!("{}", line.unwrap());
-    }
+    count(reader);
 }
