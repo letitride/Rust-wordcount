1fa4af54e58f8104e263d73eabf6446d74c57987
letitride <ichikawa.fumiya@gmail.com>
2020-11-10 11:09:10 +0900

    count関数の実装 単語数の数え上げ、結果をHashMapに格納

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ use std::env;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use regex::Regex;
+use std::collections::HashMap;
 
-fn count(input: impl BufRead) {
+fn count(input: impl BufRead) -> HashMap<String, usize> {
+    //w:単語構成文字とマッチする
     let re = Regex::new(r"\w+").unwrap();
+    let mut freqs = HashMap::new();
+
     for line in input.lines() {
         let line = line.unwrap();
         for m in re.find_iter(&line) {
-            println!("{}", m.as_str());
+            let word = m.as_str().to_string();
+            *freqs.entry(word).or_insert(0) += 1;
         }
     }
+    freqs
 }
 
 fn main() {
@@ -19,5 +25,6 @@ fn main() {
 
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(&file);
-    count(reader);
+    let freqs = count(reader);
+    println!("{:?}", freqs);
 }
