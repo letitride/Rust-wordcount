739080d65be2ee53933d6e63186dd4d279ea1452
letitride <ichikawa.fumiya@gmail.com>
2020-11-11 09:48:20 +0900

    テスト実行時のみコンパイルを行うマクロアノテーションを記述

--- src/lib.rs
+++ src/lib.rs
@@ -59,50 +59,54 @@ impl Default for CountOption {
   }
 }
 
-#[test]
-fn word_count_works() {
+#[cfg(test)]
+mod test {
   use std::io::Cursor;
+  use super::*;
 
-  let mut exp = HashMap::new();
-  exp.insert("aa".to_string(), 1);
-  exp.insert("bb".to_string(), 2);
-  exp.insert("cc".to_string(), 1);
-
-  assert_eq!(count(Cursor::new("aa bb cc bb"), CountOption::Word), exp);
-}
-
-#[test]
-fn word_count_fails() {
-  use std::io::Cursor;
-  let mut exp = HashMap::new();
-  exp.insert("aa".to_string(), 1);
+  #[test]
+  fn word_count_works() {
   
-  assert_ne!(count(Cursor::new("aa  cc dd"), CountOption::Word), exp);
-}
-
-use std::io;
-#[test]
-fn result_test() -> io::Result<()> {
-  use std::fs::{read_to_string, remove_file, write};
-
-  write("test.txt", "message")?;
-  let message = read_to_string("test.txt")?;
-  remove_file("test.txt")?;
-  assert_eq!(message, "message");
-  Ok(())
-}
-
-#[test]
-#[should_panic]
-fn word_count_do_not_contain_unknown_words() {
-  use std::io::Cursor;
-
-  count(
-    Cursor::new([
-      b'a',
-      0xf0, 0x90, 0x80,
-      0xe3, 0x81, 0x82,
-    ]),
-    CountOption::Word
-  );
+    let mut exp = HashMap::new();
+    exp.insert("aa".to_string(), 1);
+    exp.insert("bb".to_string(), 2);
+    exp.insert("cc".to_string(), 1);
+  
+    assert_eq!(count(Cursor::new("aa bb cc bb"), CountOption::Word), exp);
+  }
+  
+  #[test]
+  fn word_count_fails() {
+    use std::io::Cursor;
+    let mut exp = HashMap::new();
+    exp.insert("aa".to_string(), 1);
+    
+    assert_ne!(count(Cursor::new("aa  cc dd"), CountOption::Word), exp);
+  }
+  
+  use std::io;
+  #[test]
+  fn result_test() -> io::Result<()> {
+    use std::fs::{read_to_string, remove_file, write};
+  
+    write("test.txt", "message")?;
+    let message = read_to_string("test.txt")?;
+    remove_file("test.txt")?;
+    assert_eq!(message, "message");
+    Ok(())
+  }
+  
+  #[test]
+  #[should_panic]
+  fn word_count_do_not_contain_unknown_words() {
+  
+    count(
+      Cursor::new([
+        b'a',
+        0xf0, 0x90, 0x80,
+        0xe3, 0x81, 0x82,
+      ]),
+      CountOption::Word
+    );
+  }
 }
